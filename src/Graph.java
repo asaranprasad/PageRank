b1d098ba20f4f2b7a1e4ce418f714408fd945c0a
Saran Prasad Ambikapathy <ambikapathy.s@husky.neu.edu>
2018-03-06 11:41:27 -0500

    page rank algorithm implementation without unit tests

--- src/Graph.java
+++ src/Graph.java
@@ -3,21 +3,50 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Graph {
-  private HashMap<String, HashSet<String>> graph;
+  private HashMap<String, HashSet<String>> inLinksGraph;
+  private HashMap<String, HashSet<String>> outLinksGraph;
 
   public Graph(String filePath) {
-    graph = (new FileUtility()).textFileToGraph(filePath);
+    inLinksGraph = (new FileUtility()).textFileToGraph(filePath);
+    outLinksGraph = new HashMap<String, HashSet<String>>();
+    generateOutLinksGraph();
+  }
+
+  private void generateOutLinksGraph() {
+    for (String page : P()) {
+      HashSet<String> outlinks = new HashSet<String>();
+      for (String otherPages : P()) {
+        //        if (otherPages.equals(page))
+        //          continue;
+        if (M(otherPages).contains(page))
+          outlinks.add(otherPages);
+      }
+      outLinksGraph.put(page, outlinks);
+    }
   }
 
   public Set<String> P() {
-    return graph.keySet();
+    return inLinksGraph.keySet();
   }
 
   public int size() {
-    return graph.size();
+    return inLinksGraph.size();
   }
 
   public Set<String> M(String p) {
-    return graph.get(p);
+    return inLinksGraph.get(p);
+  }
+
+  public Set<String> S() {
+    HashSet<String> sinks = new HashSet<String>();
+    for (String page : outLinksGraph.keySet()) {
+      if (outLinksGraph.get(page).isEmpty())
+        sinks.add(page);
+    }
+    return sinks;
+  }
+
+  public int L(String q) {
+    return outLinksGraph.get(q).size();
   }
 }
--- src/PageRank.java
+++ src/PageRank.java
@@ -1,7 +1,16 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class PageRank {
 
-  public PageRank() {
+  private int CONVERGENCE_THRESHOLD = 4;
+  private double d = 0.85;
+  private List<Double> perplexity;
 
+  public PageRank(double dampingFactor) {
+    d = dampingFactor;
+    perplexity = new ArrayList<Double>();
   }
 
   //P is the set of all pages; |P| = N
@@ -9,7 +18,75 @@ public class PageRank {
   //M(p) is the set (without duplicates) of pages that link to page p
   //L(q) is the number of out-links (without duplicates) from page q
   //d is the PageRank damping/teleportation factor; use d = 0.85 as a fairly typical value
-  public float getPageRankScore() {
+  public HashMap<String, Double> calculatePageRank(Graph g) {
+    HashMap<String, Double> PR = new HashMap<String, Double>();
+    double N = (double) g.size();
 
+    // initial value
+    for (String p : g.P()) {
+      PR.put(p, 1 / N);
+    }
+
+    while (!hasConverged(PR)) {
+      double sinkPR = 0;
+
+      // calculate total sink PR
+      for (String p : g.S()) {
+        sinkPR += PR.get(p);
+      }
+
+      HashMap<String, Double> newPR = new HashMap<String, Double>();
+
+      for (String p : g.P()) {
+        // teleportation
+        newPR.put(p, (1 - d) / N);
+
+        // spread remaining sink PR evenly
+        newPR.put(p, newPR.get(p) + (d * (sinkPR / N)));
+
+        // pages pointing to p
+        for (String q : g.M(p)) {
+          // add share of PageRank from in-links
+          newPR.put(p, newPR.get(p) + (d * (PR.get(q) / (double) g.L(q))));
+        }
+      }
+
+      for (String p : g.P()) {
+        PR.put(p, newPR.get(p));
+      }
+
+      calculatePerplexity(PR);
+    }
+    return PR;
+  }
+
+  private void calculatePerplexity(HashMap<String, Double> PR) {
+    double p = Math.pow(2, H(PR));
+    perplexity.add(p);
+  }
+
+  private double H(HashMap<String, Double> PR) {
+    double perplexity = 0;
+    double log2 = Math.log(2);
+    for (String page : PR.keySet()) {
+      double xi = PR.get(page);
+      perplexity += xi * (Math.log(xi) / log2);
+    }
+    perplexity = -perplexity;
+    return perplexity;
+  }
+
+  private boolean hasConverged(HashMap<String, Double> PR) {
+    if (perplexity.size() < 4)
+      return false;
+
+    boolean returnValue = true;
+    int i = perplexity.size() - CONVERGENCE_THRESHOLD + 1;
+    while (i < perplexity.size()) {
+      if (Math.abs(perplexity.get(i - 1) - perplexity.get(i)) >= 1)
+        returnValue = false;
+      i++;
+    }
+    return returnValue;
   }
 }
